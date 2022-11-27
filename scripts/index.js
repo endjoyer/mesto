82680f748b59c001863f7d6fae0ecdc7c253377b
Aleksey Abamzarov <aleksey2308@mail.ru>
2022-11-28 00:14:43 +0300

    fixs: all errors after the first revue

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,18 @@ let popup = document.querySelector('.popup');
 let popupContainer = document.querySelector('.popup__container');
 let openPopupButtons = document.querySelectorAll('.profile__edit-batton');
 let closePopupButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector("input[name='name']");
+let jobInput = document.querySelector("input[name='about-me']");
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let submit = document.querySelector('.popup__btn');
 
 openPopupButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
     e.preventDefault();
     popup.classList.add('popup_opened');
-    popupContainer.classList.add('popup_opened');
+    popupContainer.classList.add('popup_opened'); //Такой метод реализован, для плавного открытия и закрытия попапа, другого метода реализации моей задумки, я не могу придумать
   });
 });
 
@@ -49,23 +55,15 @@ document.addEventListener('click', (e) => {
   }
 });
 
-let formElement = document.querySelector('.popup');
-let nameInput = document.querySelector("input[name='name']");
-let jobInput = document.querySelector("input[name='about-me']");
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  nameInput = nameInput.value;
-  jobInput = jobInput.value;
-
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubtitle = document.querySelector('.profile__subtitle');
-
-  profileTitle.textContent = nameInput;
-  profileSubtitle.textContent = jobInput;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-console.log(nameInput);
+submit.addEventListener('click', () => {
+  popup.classList.remove('popup_opened');
+  popupContainer.classList.remove('popup_opened');
+});
