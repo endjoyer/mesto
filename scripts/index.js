c2b24ee1f34bba47acc4e9ce44818f0b1f348566
Aleksey Abamzarov <aleksey2308@mail.ru>
2023-01-10 15:10:03 +0300

    refactor: code card in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 //------------------------------Popup_profile------------------------------
 
 const popup = document.querySelector('.popup');
-const popups = Array.from(document.querySelectorAll('.popup'));
 const popupEditProfile = document.querySelector('#popup-edit');
 const popupEditContainer = popupEditProfile.querySelector('.popup__container');
 const openPopupEditButtons = document.querySelector('.profile__edit-batton');
@@ -32,7 +31,7 @@ function closePopupByTarget(e) {
 }
 
 function сlosePopupByEscape(e) {
-  if (e.code === 'Escape' && activePopup.classList.contains('popup_opened')) {
+  if (e.code === 'Escape') {
     closePopup(activePopup);
   }
 }
@@ -66,72 +65,166 @@ const addElementForm = popupCard.querySelector('.popup__container');
 const elementsContainer = document.querySelector('.elements__container');
 const nameImgInput = addElementForm.querySelector("input[name='name-img']");
 const linkInput = addElementForm.querySelector("input[name='link']");
-const button = addElementForm.querySelector('.popup__btn');
 const popupLookImg = document.querySelector('.popup-look-img');
-const popupLookImgContainer = document.querySelector(
-  '.popup-look-img__container'
-);
 const popupLookImgTitle = document.querySelector('.popup-look-img__title');
 const popupLookImgClose = popupLookImg.querySelector('.popup__close');
 const pictureFull = document.querySelector('.popup-look-img__image');
 
-popupCardOpenButtons.addEventListener('click', () => {
-  openPopup(popupCard);
-});
-
-closePopupButtonCard.addEventListener('click', () => {
-  closePopup(popupCard);
-});
-
-popupLookImgClose.addEventListener('click', () => {
-  closePopup(popupLookImg);
-});
-
 //------------------------------Adding_cards------------------------------
-//Не получается у меня вывести submit попопа редактирования в глобальную области видимости, только в createCard. Из-за этого пришлось смириться. А насчет дополнительного функционала я с вами согласен. Просто при сдаче на ревю, ты или считаешь что у тебя более менее все корректно выполнено или у тебя по другому никак ни выходит.
+class Card {
+  constructor(name, link, templateSelector) {
+    this._name = name;
+    this._link = link;
+    this._templateSelector = templateSelector;
+  }
 
-const createCard = ({ name, link }) => {
-  const element = document
-    .querySelector('#element-template')
-    .content.querySelector('.element')
-    .cloneNode(true);
-  const elementImage = element.querySelector('.element__image');
-  const elementName = element.querySelector('.element__name');
-  elementName.textContent = name;
-  elementImage.src = link;
-  elementImage.alt = name;
-  elementImage.addEventListener('click', () => {
-    pictureFull.src = elementImage.src;
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector('.element')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+
+    this._element.querySelector('.element__image').src = this._link;
+    this._element.querySelector('.element__name').textContent = this._name;
+    this._element.querySelector('.element__image').alt = this._name;
+
+    return this._element;
+  }
+
+  _setEventListeners() {
+    popupCardOpenButtons.addEventListener('click', () => {
+      openPopup(popupCard);
+    });
+
+    closePopupButtonCard.addEventListener('click', () => {
+      closePopup(popupCard);
+    });
+
+    popupLookImgClose.addEventListener('click', () => {
+      closePopup(popupLookImg);
+    });
+    this._element
+      .querySelector('.element__image')
+      .addEventListener('click', () => {
+        this._handleOpenPopupLookImg();
+      });
+    this._element
+      .querySelector('.element__delete')
+      .addEventListener('click', () => {
+        this._handleDeleteCard();
+      });
+    this._element
+      .querySelector('.element__like')
+      .addEventListener('click', (e) => {
+        this._handleLikeCard(e);
+      });
+  }
+
+  _handleOpenPopupLookImg() {
+    pictureFull.src = this._link;
     openPopup(popupLookImg);
-    pictureFull.alt = elementImage.alt;
-    popupLookImgTitle.textContent = pictureFull.alt;
-  });
-  element.querySelector('.element__delete').addEventListener('click', () => {
-    element.remove();
-  });
-  element.querySelector('.element__like').addEventListener('click', (e) => {
+    pictureFull.alt = this._name;
+    popupLookImgTitle.textContent = this._name;
+  }
+
+  _handleDeleteCard() {
+    this._element.remove();
+  }
+  _handleLikeCard(e) {
     e.target.classList.toggle('element__like_active');
+  }
+}
+
+const renderCards = () => {
+  initialCards.forEach((item) => {
+    const cards = new Card(item.name, item.link, '#element-template');
+    const cardElement = cards.generateCard();
+
+    elementsContainer.append(cardElement);
   });
 
-  return element;
+  const addCard = (e) => {
+    e.preventDefault();
+    const name = nameImgInput.value;
+    const link = linkInput.value;
+    const card = new Card(name, link, '#element-template');
+    const cardElement = card.generateCard();
+    nameImgInput.value = '';
+    linkInput.value = '';
+    elementsContainer.prepend(cardElement);
+    closePopup(popupCard);
+  };
+
+  addElementForm.addEventListener('submit', addCard);
 };
 
-const renderCard = ({ name, link }) => {
-  elementsContainer.prepend(createCard({ name, link }));
-};
+renderCards();
 
-elementsContainer.append(...initialCards.map(createCard));
+// const renderCard = ({ name, link }) => {
+//   elementsContainer.prepend(createCard({ name, link }));
+// };
 
-const addElement = (e) => {
-  e.preventDefault();
-  const name = nameImgInput.value;
-  const link = linkInput.value;
-  nameImgInput.value = '';
-  linkInput.value = '';
-  renderCard({ name, link });
-  closePopup(popupCard);
-};
+// const addElement = (e) => {
+//   e.preventDefault();
+//   const name = nameImgInput.value;
+//   const link = linkInput.value;
+//   nameImgInput.value = '';
+//   linkInput.value = '';
+//   renderCard({ name, link });
+//   closePopup(popupCard);
+// };
 
-addElementForm.addEventListener('submit', addElement);
+// addElementForm.addEventListener('submit', addElement);
 
-enableValidation(validationConfig);
+// const createCard = ({ name, link }) => {
+//   const element = document
+//     .querySelector('#element-template')
+//     .content.querySelector('.element')
+//     .cloneNode(true);
+//   const elementImage = element.querySelector('.element__image');
+//   const elementName = element.querySelector('.element__name');
+//   elementName.textContent = name;
+//   elementImage.src = link;
+//   elementImage.alt = name;
+//   elementImage.addEventListener('click', () => {
+//     pictureFull.src = elementImage.src;
+//     openPopup(popupLookImg);
+//     pictureFull.alt = elementImage.alt;
+//     popupLookImgTitle.textContent = pictureFull.alt;
+//   });
+//   element.querySelector('.element__delete').addEventListener('click', () => {
+//     element.remove();
+//   });
+// element.querySelector('.element__like').addEventListener('click', (e) => {
+//   e.target.classList.toggle('element__like_active');
+// });
+
+//   return element;
+// };
+
+// const renderCard = ({ name, link }) => {
+//   elementsContainer.prepend(createCard({ name, link }));
+// };
+
+// elementsContainer.append(...initialCards.map(createCard));
+
+// const addElement = (e) => {
+//   e.preventDefault();
+//   const name = nameImgInput.value;
+//   const link = linkInput.value;
+//   nameImgInput.value = '';
+//   linkInput.value = '';
+//   renderCard({ name, link });
+//   closePopup(popupCard);
+// };
+
+// addElementForm.addEventListener('submit', addElement);
+
+// enableValidation(validationConfig);
